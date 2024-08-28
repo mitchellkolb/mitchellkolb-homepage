9a9adb89ebb3c14950e41064db996c7a7c961ea5
mitchellkolb <mkolb321@gmail.com>
2024-08-28 05:22:51 -0700

    Completed the Pi Music Player item in the hardware section

--- pages/hardware/pi-music.js
+++ pages/hardware/pi-music.js
@@ -12,38 +12,39 @@ import {
   import Layout from '../../components/layouts/article'
   
   const Hardware = () => (
-    <Layout title="Inkdro33p">
+    <Layout title="Music Player">
       <Container>
         <Title>
         Raspberry Pi Music Player <Badge>Ongoing</Badge>
         </Title>
         <P>
-          I built a music player that boots off of a raspberry pi 3B+ that opens a browser using playwright and plays music from http://pianostream.com/ with custom GUI audio controls built with the Kivy UI Python UI library. All of this is placed within a 3D printed case that has openings for all ports and the 5 inch touchscreen.
+          I'm building a music player that boots off of a Raspberry Pi 3B+ that opens a browser using playwright and plays music from pianostream.com with a custom GUI audio controls built with the Kivy UI Python UI library. All of this is going to be placed within a 3D printed case that has openings for all ports and the 5 inch touchscreen. This project will be made public when it is completed and this page will be updated with more images when its in a working condition.
         </P>
         <List ml={4} my={4}>
           <ListItem>
-            <Meta>Website</Meta>
-            <Link href="https://www.inkdrop.app/">
-              https://www.inkdrop.app/ <ExternalLinkIcon mx="2px" />
+            <Meta>Music</Meta>
+            <Link href="http://pianostream.com/" isExternal>
+              pianostream.com <ExternalLinkIcon mx="2px" />
             </Link>
           </ListItem>
           <ListItem>
-            <Meta>Platform</Meta>
-            <span>Windows/macOS/Linux/iOS/Android</span>
+            <Meta>UI Library</Meta>
+            <Link href="https://kivy.org/" isExternal>
+              Kivy.org <ExternalLinkIcon mx="2px" />
+            </Link>          
+          </ListItem>
+          <ListItem>
+            <Meta>Web Automation</Meta>
+            <Link href="https://playwright.dev/" isExternal>
+              playwright.dev <ExternalLinkIcon mx="2px" />
+            </Link>          
           </ListItem>
         </List>
   
-        <HardwareImage src="/images/works/inkdrop_01.png" alt="Inkdrop" />
-        <HardwareImage src="/images/works/inkdrop_02.png" alt="Inkdrop" />
-        <AspectRatio maxW="640px" ratio={1.7} my={4}>
-          <iframe
-            src="https://www.youtube.com/embed/-qBavwqc_mY"
-            title="YouTube video player"
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          ></iframe>
-        </AspectRatio>
+        <HardwareImage src="/images/hardware/pi-music/pi-music-1.jpeg" alt="pi-music-1.jpeg" />
+        <HardwareImage src="/images/hardware/pi-music/pi-music-2.jpg" alt="pi-music-2.jpg" />
+
+
       </Container>
     </Layout>
   )
